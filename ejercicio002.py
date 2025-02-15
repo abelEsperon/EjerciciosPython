1775a41284747e81ec1bbedf722fa2dbf0be999b
abelGonzalez <abel21ge@gmail.com>
2025-02-15 16:37:03 -0600

    Agregacion de dos archivos de tipo python

---
+++ ejercicio002.py
@@ -0,0 +1,10 @@
+# Ejemplo de un ciclo while en Python
+
+contador = 0
+
+while contador < 5:
+    print(f"El contador es: {contador}")
+    contador += 1
+
+contador2 = 0
+# Aqui se realizaron mas cambios.
