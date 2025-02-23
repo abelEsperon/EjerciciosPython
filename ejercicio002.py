38f15212e47a1f81ccb601235ed6ce63d6d2300d
abel Gonzalez <abel21ge@gmail.com>
2025-02-22 22:12:00 -0600

    jugando con git

--- ejercicio002.py
+++ ejercicio002.py
@@ -8,3 +8,10 @@ while contador < 5:
 
 contador2 = 0
 # Aqui se realizaron mas cambios.
+print("Hello git world")
+
+
+bubblesex
+
+
+(@(& 'C:/Users/user/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\user\AppData\Local\Programs\oh-my-posh\themes\bubblesextra.omp.json' --print) -join "`n") 
